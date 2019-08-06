a3e667b1c4d2405ba6c9c25074b3915076ae33bc
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2019-08-06 11:54:33 -0500

    Updates

---
+++ precache-manifest.88f4006b2eb6dfd5e6f5e5879df4468f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f616a491c8e50d0357a51a6f7e627567",
+    "url": "/bass-selector/index.html"
+  },
+  {
+    "revision": "0ce0d6e8be8b29c62985",
+    "url": "/bass-selector/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "0ce0d6e8be8b29c62985",
+    "url": "/bass-selector/static/js/2.0fd8de87.chunk.js"
+  },
+  {
+    "revision": "d73d5695749ad8ff2507",
+    "url": "/bass-selector/static/js/main.08992828.chunk.js"
+  },
+  {
+    "revision": "aae0f9b9e12c60df40ac",
+    "url": "/bass-selector/static/js/runtime~main.eb9821e6.js"
+  }
+]);
